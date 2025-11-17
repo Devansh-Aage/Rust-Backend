97e33cfda350d79664cb4a40f909bab9f91c29cc
Devansh-Aage <devanshaage@gmail.com>
2025-11-17 16:05:57 +0000

    week2: Project done

--- week2/src/error.rs
+++ week2/src/error.rs
@@ -4,6 +4,7 @@ use axum::{
     response::{IntoResponse, Response},
 };
 use serde::Serialize;
+use serde_json::json;
 use thiserror::Error;
 
 #[derive(Debug, Clone, Copy)]
@@ -20,7 +21,11 @@ pub enum AppError {
     #[error("Bad request: {0}")]
     BadRequest(String),
 
-    #[error("Invalid ID: {id},expected atleast {} and atmost {}", .limit.li, .limit.hi)] // `.` used cause limit is nested 
+    #[error("Validation Error: {0}")]
+    ValidationError(serde_valid::validation::Errors),
+
+    #[error("Invalid ID: {id},expected atleast {} and atmost {}", .limit.li, .limit.hi)]
+    // `.` used cause limit is nested
     OutOfBounds { id: u32, limit: Limit },
 
     #[error("Something went wrong")]
@@ -28,22 +33,65 @@ pub enum AppError {
 }
 
 #[derive(Serialize)]
-struct ErrorResponse {
-    error: String,
+enum ErrorResponse {
+    Message {
+        error: String,
+    },
+    Validation {
+        error: String,
+        details: serde_json::Value,
+    },
 }
 
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
-        let status = match self {
-            AppError::BadRequest(_) => StatusCode::BAD_REQUEST,
-            AppError::NotFound => StatusCode::NOT_FOUND,
-            AppError::Internal(_) => StatusCode::INTERNAL_SERVER_ERROR,
-            AppError::OutOfBounds { id, limit } => StatusCode::BAD_REQUEST,
-        };
+        match self {
+            AppError::ValidationError(errs) => {
+                let json = serde_json::to_value(&errs)
+                    .unwrap_or(json!({"error":"Failed to serialize validation error to json"}));
 
-        let body = Json(ErrorResponse {
-            error: self.to_string(),
-        });
-        (status, body).into_response()
+                (
+                    StatusCode::BAD_REQUEST,
+                    Json(ErrorResponse::Validation {
+                        error: "Validation Error".into(),
+                        details: json,
+                    }),
+                )
+                    .into_response()
+            }
+
+            AppError::BadRequest(msg) => (
+                StatusCode::BAD_REQUEST,
+                Json(ErrorResponse::Message { error: msg }),
+            )
+                .into_response(),
+
+            AppError::NotFound => (
+                StatusCode::NOT_FOUND,
+                Json(ErrorResponse::Message {
+                    error: "Resource not found".into(),
+                }),
+            )
+                .into_response(),
+
+            AppError::OutOfBounds { id, limit } => (
+                StatusCode::BAD_REQUEST,
+                Json(ErrorResponse::Message {
+                    error: format!(
+                        "Invalid ID {id}, must be between {} and {}",
+                        limit.li, limit.hi
+                    ),
+                }),
+            )
+                .into_response(),
+
+            AppError::Internal(_) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(ErrorResponse::Message {
+                    error: "Internal Server Error".into(),
+                }),
+            )
+                .into_response(),
+        }
     }
 }
--- week2/src/handlers/body.rs
+++ week2/src/handlers/body.rs
@@ -1,17 +1,35 @@
-use axum::Json;
+use axum::{Json};
 use serde::{Deserialize, Serialize};
 use serde_json::{Value, json};
+use serde_valid::Validate;
+use serde_valid::validation::Error;
 
 use crate::error::{AppError, Limit};
 
-#[derive(Serialize, Deserialize)]
+fn validate_email(value: &str) -> Result<(), Error> {
+    if value.contains("@") {
+        Ok(())
+    } else {
+        Err(Error::Custom(
+            "email field must be a valid Email".to_string(),
+        ))
+    }
+}
+
+#[derive(Serialize, Deserialize, Validate)]
 pub struct Creds {
+    #[validate(minimum = 1)]
     pub id: u32,
+    #[validate(min_length = 3)]
     pub name: String,
+    #[validate(custom= validate_email)]
     pub email: String,
 }
 
 pub async fn parse_body(Json(payload): Json<Creds>) -> Result<Json<Value>, AppError> {
+    if let Err(errs) = payload.validate() {
+        return Err(AppError::ValidationError(errs));
+    }
     let id = payload.id;
     let name = payload.name;
     let email = payload.email;
